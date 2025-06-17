b48924a34619601e0dd7a105076347368c083367
邹子辰的小MAC😋 <zzcnb123456@MacBook-Pro.local>
2025-06-17 15:20:44 +0800

    can play now

--- Assets/PianoKey2.cs
+++ Assets/PianoKey2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine;
 
-public class PianoKey : PushKey
+public class PianoKey2 : PushKey
 {
     [SerializeField]
     KeySequenceControl controller;
@@ -11,18 +11,21 @@ public class PianoKey : PushKey
     public Renderer renderer;
 
     public int key_id;
+    private Vector3 initialLocalPosition;
+    public float pressDistance = 0.01f;
     public void Start(){
         controller = GetComponentInParent<KeySequenceControl>();
-
+        initialLocalPosition = transform.localPosition;
     }
     public void Push(){
         base.Push();
         controller.Push(key_id);
-
+        transform.localPosition = initialLocalPosition + new Vector3(0, 0, -pressDistance);
     }
 
     public void Release(){
         base.Release();
-
+        transform.localPosition = initialLocalPosition;
     }
 }
+
